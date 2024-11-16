c68d0daecdeed4cd81e4b1fbc977f8478b9d2c43
VD3baeKy <digfehc@gmail.com>
2024-11-16 20:27:07 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,10 +1,11 @@
 let num = 18;
-if(num%3==0.0 && num%5!=0.0){
+//if(num%3==0.0 && num%5!=0.0){ //%は余りを出す=小数とはならない
+if(num%3==0 && num%5!=0){
    console.log("3の倍数です");
-}else if(num%3!=0.0 && num%5==0.0){
+//}else if(num%3!=0.0 && num%5==0.0){ //%は余りを出す=小数とはならない
+}else if(num%3!=0 && num%5==0){
    console.log("5の倍数です");
-}else if(num%3==0.0 && num%5==0.0){
+//}else if(num%3==0.0 && num%5==0.0){ //%は余りを出す=小数とはならない
+}else if(num%3==0 && num%5==0){
    console.log("3と5の倍数です");
 }
-
-
